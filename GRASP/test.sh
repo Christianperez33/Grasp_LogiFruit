0d4dc410428c4afbdd4e2cd97fd1ae6fe886b618
christianperez33 <christianperez33@gmail.com>
2020-12-10 10:18:52 +0100

    .

--- GRASP/test.sh
+++ GRASP/test.sh
@@ -5,7 +5,7 @@ do
     for a in {50..100..10}
     do
         echo -ne "Progress Iter: $[ (100/5)*($i) ]% - Progress Alfa: $[ $a*100/100 ]% \n";
-        python3 main.py -i $i -av $a  -m 2 -g True -t True -x ./data/StockSolucionResolver.csv -y ./data/viajes_zonas.xml --debug True
+        python3 main.py -i $i -av $a  -m 2 -g True -t True -x ../data/StockSolucionResolver.csv -y ../data/viajes_zonas.xml --debug True
     done
 done
 end=`date +%s`
